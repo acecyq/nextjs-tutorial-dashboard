4a30ee3223338b5550820b7c5c76dda01c949019
acecyq <acecyq@gmail.com>
2025-05-11 22:38:06 +0800

    Stream RevenueChart

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,13 +1,19 @@
-import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '@/app/lib/data';
-import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import { lusitana } from '@/app/ui/fonts';
-import { Card } from '../../ui/dashboard/cards';
-import LatestInvoices from '../../ui/dashboard/latest-invoices';
+import { fetchCardData, fetchLatestInvoices } from "@/app/lib/data";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import { lusitana } from "@/app/ui/fonts";
+import { RevenueChartSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+import { Card } from "../../ui/dashboard/cards";
+import LatestInvoices from "../../ui/dashboard/latest-invoices";
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
-  const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData();
+  const {
+    totalPaidInvoices,
+    totalPendingInvoices,
+    numberOfInvoices,
+    numberOfCustomers,
+  } = await fetchCardData();
 
   return (
     <main>
@@ -25,7 +31,9 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
